2473ed5198d5706e8fbac64ade40ff103c861cf4
Lloyd Bill <lloydbill.lariosa@gmail.com>
2024-05-18 16:51:08 +0800

    remove some styling that was ruining the behavior of my code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const startButton = document.querySelector(".start-button");
 const changeButton = document.querySelector(".change-button"); 
 
 
-startButton.addEventListener("click", createGrid(16,16));
+startButton.addEventListener("click", () => createGrid(16));
 changeButton.addEventListener("click", () => {
 
   let userInput = prompt("Input size of grid from 1-100:");
@@ -27,19 +27,28 @@ changeButton.addEventListener("click", () => {
 
 function createGrid(gridWidth) {
 
-  let gridArea = gridWidth * gridWidth;
+  containerDiv.innerHTML = '';
+
+  const containerWidth = parseFloat(getComputedStyle(containerDiv).width); // Get container width
+
+  const cellSize = containerWidth / gridWidth;
+
+  let gridArea = gridWidth * gridWidth ;
 
   for ( let i = 0 ; i < gridArea; i++ ) {
 
-    const grid = document.createElement("div");
+    const cell = document.createElement("div");
   
-    grid.classList.add("grid");
+    cell.classList.add("cell");
   
-    containerDiv.appendChild(grid);
+    containerDiv.appendChild(cell);
+
+    cell.style.width = `${cellSize}px`;
+    cell.style.height = `${cellSize}px`;
   
-    grid.addEventListener('mouseenter', function(event) {
-      // This function will be triggered when the mouse enters the grid
-      grid.classList.add("hover-on");
+    cell.addEventListener('mouseenter', function(event) {
+      // This function will be triggered when the mouse enters the cell
+      cell.classList.add("hover-on");
     });
   
   }
